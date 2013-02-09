a417ad75cd70dea9a1a6517e7fc56f9ede168d52
mohit310 <mohit.keswani@gmail.com>
2013-02-08 22:34:02 -0500

    Test class

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mohit
+ * Date: 2/8/13
+ * Time: 10:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
